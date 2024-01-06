6f83711ce7b8f5fa16f6acf8c5b73ca511b2091d
nasreenmalik <147983899+nanenmalik@users.noreply.github.com>
2024-01-07 01:21:26 +0800

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,30 @@
+class Account {
+        protected double balance;
+    
+        public Account(double init_balance) {
+            this.balance = init_balance;
+           
+        }
+    
+        public double getBalance() {
+            return balance;
+        }
+    
+        public boolean deposit(double amount) {
+            if (amount > 0) {
+                balance += amount;
+                return true;
+            }
+            return false;
+        }
+    
+        public boolean withdraw(double amount) {
+            if (amount > 0 && amount <= balance) {
+                balance -= amount;
+                return true;
+            }
+            return false;
+        }
+
+}
+    
