ef82a02dadab642d3cbdab504ad0e11c1b2ea59e
nasreenmalik <147983899+nanenmalik@users.noreply.github.com>
2024-01-07 01:24:38 +0800

    Update CheckingAccount.java

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -3,19 +3,13 @@
 class CheckingAccount extends Account{
 
     private double insufficientFee;
-    //double overdraftFee = 10.00;
+    
     public CheckingAccount(double init_balance, double insufficientFee) {
         super(init_balance);
         this.insufficientFee = insufficientFee;
     }
 
-   /*  public void overdraftFee(double withdrawAmount) {
-        if(withdrawAmount > balance) {
-            balance = balance - overdraftFee;
-        }
-    }*/
-
-    @Override
+      @Override
     public boolean withdraw(double amount) {
         if (amount <= balance) {
             balance -= amount;
@@ -36,36 +30,4 @@ class CheckingAccount extends Account{
 
 
 
-    /*double overdraftFee = 10.00;
-
-    private double insufficientFee;
-
-    public CheckingAccount( double balance, double insufficientFee) {
-        super(balance);
-        this.insufficientFee = insufficientFee;
-    }
-
-    public overdraftFee() {
-        if(withdrawAmount > balance) {
-            balance = balance - overdraftFee;
-        }
-    }
-
-
-    @Override
-    public boolean withdraw (double amount) {
-        if (amount > getBalance()) {
-            System.out.println("Insufficient funds");
-            return false;
-        } else {
-            setBalance(getBalance() - amount);
-            return true;
-        }
-        if (amount > getBalance()) {
-            super.withdraw(insufficientFee);
-        }
-        super.withdraw(amount);
-    }
-
-
-}*/
+  
